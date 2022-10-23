6ed15d338017fbc813aeba207bac86ee7e00007f
Israel <israely@insightec.com>
2022-10-23 20:07:35 +0300

    add command argument (mode) mode = 0: update emmc only mode = 1: configure ip and mac address

--- upgrade.py
+++ upgrade.py
@@ -3,7 +3,7 @@ import paramiko
 import ntpath
 from datetime import datetime
 import threading
-
+import sys
 
 maps_list = [['172.16.0.10', 22, 'root', 'root', '172.16.1.20'],
              ['172.16.0.11', 22, 'root', 'root', '172.16.1.21']]
@@ -26,20 +26,27 @@ class SystemUpgrade:
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.ssh.connect(ip, port, uname, passwd)
         if self.ssh:
-            self.log.info("ssh connection succeed")
+            self.log.info("ssh connection to {} succeed".format(ip))
             return True
         else:
-            self.log.error("ssh connection failed")
+            self.log.error("ssh connection to {} failed".format(ip))
             return False
 
     def ssh_disconnect(self):
         self.ssh.close()
         self.log.info("ssh connection disconnected")
 
-    def run_script(self, ip):
-        self.log.info("running update script")
-        cmd = 'sh update_emmc.sh '+ str(ip)
-        self._run_command(cmd)
+    def run_script(self, ip, mode):
+        self.log.info("running update script on machine {}".format(ip))
+        if (mode != 0):
+            mac = ip
+            self.log.info("running update script on machine {}".format(ip))
+            cmd = 'sh update_mac.sh ' + str(ip) + ' '+ str(mac)
+            print(cmd)
+        else:
+            cmd = 'sh update_emmc.sh'
+            print(cmd)
+       self._run_command(cmd)
 
     def terminate(self):
         pass
@@ -52,26 +59,26 @@ class SystemUpgrade:
             self.log.info(str(line))
 
 
-def upgrade_func(map):
+def upgrade_func(map, mode):
     upgrade = SystemUpgrade()
     ip = map[0]
     port = map[1]
     uname = map[2]
     passwd = map[3]
     new_ip = map[4]
-    
+
     print("start updating machine #{}".format(ip))
-    
+
     if not upgrade.ssh_connect(ip, port, uname, passwd):
         print("failed connect to the server")
-    
+
     print("runnig emmc upgrade's script")
-    if(upgrade.run_script(new_ip)):
+    if upgrade.run_script(new_ip, mode):
         print("failed to run script")
-    
+
     upgrade.ssh_disconnect()
     print("machine upgrade finished\n")
-    
+
     upgrade.terminate()
     print("system upgrade finished\n")
 
@@ -79,10 +86,18 @@ def upgrade_func(map):
 def main():
     thread_list = []
     print("starting system upgrade. log file @upgrade.log")
+    num_of_arg = len(sys.argv)
 
-    for map in maps_list:
+    if num_of_arg < 2:
+        print("ERROR: missing MODE argument. please run with mode (0 - for update emmc, 1- for update ip and mac)")
+        sys.exit()
+
+    mode =  int(sys.argv[1])
+    print("mode = {}".format(mode))
+
+    for map_target in maps_list:
         print("creating thread..")
-        t = threading.Thread(target=upgrade_func, args=(map,))
+        t = threading.Thread(target=upgrade_func, args=(map_target, mode))
         thread_list.append(t)
 
     for thread in thread_list:
@@ -92,7 +107,7 @@ def main():
     for thread in thread_list:
         print("waiting for thread to finish..")
         thread.join()
-        
+
 
 if __name__ == "__main__":
     main()
